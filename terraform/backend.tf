ee7d01b5764f9b2b5f6e854a45598f5369dd97cd
Nathan-Roberts123 <robertsnathan2004@gmail.com>
2026-07-02 10:38:09 +0200

    terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "flux-tutorial-three-terraform-state-twg253"
+    bucket         = "flux-self-hosting-terraform-state-twg253"
     key            = "flux-bootstrap/terraform.tfstate"
     region         = "eu-west-1"
     use_lockfile   = true
